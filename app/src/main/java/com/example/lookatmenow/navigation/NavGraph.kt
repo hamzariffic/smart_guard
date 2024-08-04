6c2b00e95732e55595536b1e033698a08bdc803b
hamzariffic <hamza.mattea@gmail.com>
2024-08-04 05:40:22 +0300

    Here the NavHost is designed to handle the navigation graphl The SplashScreen pops out of back stack when 4 mins elapse

--- app/src/main/java/com/example/lookatmenow/navigation/NavGraph.kt
+++ app/src/main/java/com/example/lookatmenow/navigation/NavGraph.kt
@@ -1,2 +1,38 @@
 package com.example.lookatmenow.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.lookatmenow.views.HomeScreen
+import com.example.lookatmenow.views.SplashScreen
+
+@Composable
+fun SetupNavGraph(modifier: Modifier = Modifier,
+                  navController: NavHostController,
+                  startDestination: String = Screen.SplashScreen.route
+) {
+    NavHost(navController = navController,
+        startDestination = startDestination)
+    {
+        composable(Screen.SplashScreen.route) {
+            SplashScreen(
+                modifier = Modifier,
+                onSplashComplete = {
+                    navController.navigate(Screen.HomeScreen.route) {
+                        popUpTo(Screen.SplashScreen.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+        composable(Screen.HomeScreen.route) {
+            HomeScreen(
+                modifier = Modifier,
+                navHostController = navController
+            )
+        }
+
+    }
+
+}
