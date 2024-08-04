32485a5bbf41052bdf050034192e26ba6ad6fe47
hamzariffic <hamza.mattea@gmail.com>
2024-08-04 05:37:35 +0300

    Fixed splashscreen persists after the 4s time delay imposed. The SplashScreen was extending because I'd called it in the MainActivity when testing the feature

--- app/src/main/java/com/example/lookatmenow/MainActivity.kt
+++ app/src/main/java/com/example/lookatmenow/MainActivity.kt
@@ -9,19 +9,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.lookatmenow.navigation.Screen
+import com.example.lookatmenow.navigation.SetupNavGraph
 import com.example.lookatmenow.ui.theme.LookAtMeNowTheme
 import com.example.lookatmenow.views.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // splash screen
         installSplashScreen()
+//Here we're declaring the navController as a type of NavHostController
+        lateinit var navController: NavHostController
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             LookAtMeNowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SplashScreen(modifier = Modifier.padding(innerPadding))
-                }
+//                This is where we're passing the navController to the HomeScreen and it'll help in the navigation
+//               We also call the SetupNavGraph function here since it will help us in the navigation
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
