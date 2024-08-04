7036e38f2be775db66c78ecbaa64b9b18690a8e6
hamzariffic <hamza.mattea@gmail.com>
2024-08-04 05:45:51 +0300

    Home Screen with camera card, lazy row with some buttons, and lazycolumn for populating Items

--- app/src/main/java/com/example/lookatmenow/views/HomeScreen.kt
+++ app/src/main/java/com/example/lookatmenow/views/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.example.lookatmenow.views
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,35 +11,43 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 
 @Composable
 fun HomeScreen(
-    modifier: Modifier
+    modifier: Modifier,
+    navHostController: NavHostController
 ){
     Column(
         modifier
             .fillMaxSize()
-//            .background(MaterialTheme.colorScheme.secondary, MaterialTheme.shapes.extraLarge)
-            .padding(24.dp),
-        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            .background(colorScheme.secondary, MaterialTheme.shapes.extraLarge)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
             modifier
-                .padding(16.dp)
-                .aspectRatio(2f)
-                .background(MaterialTheme.colorScheme.primary)
+                .padding(18.dp)
+                .aspectRatio(1f)
+                .background(colorScheme.onTertiary, MaterialTheme.shapes.extraLarge)
                 .fillMaxWidth()
-            ) {
-//            Display individuals at the entrance
-            Text(text = "Visitor on the door!", modifier = Modifier.align(androidx.compose.ui.Alignment.Center),
-                textAlign = androidx.compose.ui.text.style.TextAlign.Center)
+        ) {
+    //            Display individuals at the entrance
+            Text(text = "Visitor on the door!", modifier = Modifier.align(Alignment.Center),
+                textAlign = TextAlign.Center,
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,)
 
         }
         Spacer(modifier = Modifier.height(8.dp) )
@@ -49,8 +56,14 @@ fun HomeScreen(
             modifier
                 .fillMaxWidth()
                 .padding(18.dp)
-                .aspectRatio(2f)
-                .background(MaterialTheme.colorScheme.onTertiary)) {
+                .aspectRatio(4f)
+                .background(colorScheme.tertiary, MaterialTheme.shapes.extraLarge)) {
+            @Composable
+            fun FilledTonalButtonExample(onClick: () -> Unit) {
+                FilledTonalButton(onClick = { onClick() }) {
+                    Text("Tonal")
+                }
+            }
 
         }
         Spacer(modifier = Modifier.height(8.dp))
@@ -58,17 +71,17 @@ fun HomeScreen(
         LazyColumn(
             Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.tertiary, MaterialTheme.shapes.extraLarge)
+                .background(colorScheme.tertiary, MaterialTheme.shapes.extraLarge)
                 .padding(18.dp)) {
 
         }
-
-
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(Modifier)
+    HomeScreen(Modifier, navHostController = rememberNavController())
 }
