05a0bad242dad2e4390cb7b0021f74ba30a4550f
hamzariffic <hamza.mattea@gmail.com>
2024-08-06 02:37:17 +0300

    Defining the items in a BottomBar

--- app/src/main/java/com/example/lookatmenow/domain/BottomBarItems.kt
+++ app/src/main/java/com/example/lookatmenow/domain/BottomBarItems.kt
@@ -1,2 +1,11 @@
 package com.example.lookatmenow.domain
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val name: String,
+    val selectedIcon: ImageVector,
+    val unSelectedIcon: ImageVector,
+    val hasNotification: Boolean,
+    val badgeCount: Int? = null
+)
