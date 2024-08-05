12743f802476f6bb4f58a6519e116a1b93a90928
hamzariffic <hamza.mattea@gmail.com>
2024-08-06 02:11:51 +0300

    Added Settings to the sealed screen class

--- app/src/main/java/com/example/lookatmenow/navigation/SealedClass.kt
+++ app/src/main/java/com/example/lookatmenow/navigation/SealedClass.kt
@@ -1,12 +1,5 @@
 package com.example.lookatmenow.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.ui.graphics.vector.ImageVector
-
 // Sealed Class containing all the screens in this application
 //They are basically data objects, with names, and have an argument for the route as strings in the brackets
 //For this to work, we need a navController initiated including in the Main activity
@@ -14,11 +7,5 @@ sealed class Screen(val route: String) {
     data object HomeScreen: Screen("home_screen")
     data object SplashScreen: Screen("splash_screen")
     data object Notifications: Screen("notifications_screen")
-    data object Profile: Screen("profile_screen")
-}
-
-sealed class BottomNavItem(val route: String, val icon: ImageVector, val label: String) {
-    data object HomeScreen : BottomNavItem("home", Icons.Default.Home, "Home")
-    data object Notifications : BottomNavItem("notifications", Icons.Default.Notifications, "Search")
-    data object Profile : BottomNavItem("profile", Icons.Default.Person, "Profile")
+    data object Settings : Screen("settings_screen")
 }
